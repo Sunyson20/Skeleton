d3b890c4316183af2d0a4764cf3fc5d391679560
sunyson20 <sunyson.zeng@gmail.com>
2018-07-03 18:43:21 +0800

    add #main id on the first level container

--- js/site.js
+++ js/site.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   // Variables
   var $codeSnippets = $('.code-example-body'),
+      $main = $('#main')
       $nav = $('.navbar'),
       $body = $('body'),
       $window = $(window),
@@ -61,17 +62,17 @@ $(document).ready(function() {
 });
 
   function resize() {
-    $body.removeClass('has-docked-nav')
+    $main.removeClass('has-docked-nav')
     navOffsetTop = $nav.offset().top
     onScroll()
   }
 
   function onScroll() {
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
-      $body.addClass('has-docked-nav')
+    if(navOffsetTop < $window.scrollTop() && !$main.hasClass('has-docked-nav')) {
+      $main.addClass('has-docked-nav')
     }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-      $body.removeClass('has-docked-nav')
+    if(navOffsetTop > $window.scrollTop() && $main.hasClass('has-docked-nav')) {
+      $main.removeClass('has-docked-nav')
     }
   }
 
